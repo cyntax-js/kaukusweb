ec295f3ed27dda950177e9214bb60384c73ac69c
CYNTAX <samuelify225@gmail.com>
2026-01-17 13:59:08 +0100

    update

--- src/broker-theme/pages/MarketsPage.tsx
+++ src/broker-theme/pages/MarketsPage.tsx
@@ -255,6 +255,10 @@ const MarketsPage = () => {
     );
   }
 
+  console.log("====================================");
+  console.log(config, "activeMarketType");
+  console.log("====================================");
+
   return (
     <div className="min-h-screen flex flex-col bg-gray-50/80">
       <AppHeader />
